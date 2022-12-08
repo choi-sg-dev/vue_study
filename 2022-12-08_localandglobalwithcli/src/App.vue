fd4e714631a50c7127173fca921515b28355f726
EZ-sgchoi <csg1334@gmail.com>
2022-12-08 12:01:20 +0900

    cli 전역/지역 component 연습

--- 2022-12-08_localandglobalwithcli/src/App.vue
+++ 2022-12-08_localandglobalwithcli/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <child-component></child-component>
+    <comp-article></comp-article>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChildComponent from './components/ChildComponent.vue'
+import CompArticle from './components/CompArticle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'child-component': ChildComponent,
+    'comp-article': CompArticle
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 2022-12-08_localandglobalwithcli/src/components/CompArticle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam molestiae reiciendis at incidunt eos nesciunt beatae ipsum, distinctio id illo sequi, odit necessitatibus possimus ullam laudantium fuga recusandae repudiandae veritatis!
+    <child-component></child-component>
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue'
+export default {
+  component: {
+    'child-component' : ChildComponent
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    background: #00897b;
+    color: white;
+    padding:  10px 20px;
+  }
+</style>
