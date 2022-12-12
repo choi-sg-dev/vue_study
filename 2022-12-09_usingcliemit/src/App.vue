7f5118f7d1617f79896caf33eb6ce2b5afa5e439
sgchoi-dev <choi.sg.dev@gmail.com>
2022-12-12 11:44:21 +0900

    2022-12-22 vue 스터디

---
+++ 2022-12-09_usingcliemit/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    {{ name }}
+    <child-component v-on:sendmessage="receivemessage"></child-component>
+  </div>
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      name: 'emit을 이용한 데이터 전달'
+    }
+  },
+  components: {
+    'child-component': ChildComponent
+  },
+  methods: {
+    receivemessage(msg){
+      this.name = msg;
+    }
+  },
+}
+</script>
+
+<style>
+</style>
