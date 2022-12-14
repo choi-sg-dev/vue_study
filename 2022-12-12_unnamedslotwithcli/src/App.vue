b14d2f96278f3663a162e93ff8959fbe29969d28
sgchoi-dev <choi.sg.dev@gmail.com>
2022-12-14 14:00:04 +0900

    vue 공부

---
+++ 2022-12-12_unnamedslotwithcli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <child-com>
+      <p>파리(Paris)에 가면 이른 저녁에 에펠탑 구경을 추천한다.</p>
+    </child-com>
+  </div>
+</template>
+
+<script>
+import ChildCom from './components/ChildCom.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'child-com': ChildCom
+  }
+}
+</script>
+
+<style>
+</style>
