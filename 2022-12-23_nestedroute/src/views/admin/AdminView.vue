e2686489fee3e106490cd67379ed078f950d4c80
sgchoi-dev <choi.sg.dev@gmail.com>
2022-12-28 10:02:58 +0900

    named view 스터디

--- 2022-12-23_nestedroute/src/views/admin/AdminView.vue
+++ 2022-12-23_nestedroute/src/views/admin/AdminView.vue
@@ -3,8 +3,9 @@
     <h3>{{title}}</h3>
 
     <ul class="white">
-      <li><router-link to="/admin/list">제품 리스트(Product List)</router-link> | </li>
-      <li><router-link to="/admin/insert">제품 추가(New Product)</router-link></li>
+      <li><router-link :to="{name: 'productlist'}">제품 리스트(Product List)</router-link> | </li>
+      <li><router-link :to="{name: 'productinsert'}">제품 추가(New Product)</router-link></li>
+      <li><router-link :to="{name: 'productedit', params: {id: 'p01'}}">제품 수정(Edit Product)</router-link></li>
     </ul>
 
     <router-view></router-view> <!-- children 라우트 -->
