7dd4aa7c60c7cdd7519ace38db02ac96c5c44a51
sgchoi-dev <choi.sg.dev@gmail.com>
2022-12-29 13:56:54 +0900

    named view 공부

--- 2022-12-22_routingfirst/src/views/HomeView.vue
+++ 2022-12-22_routingfirst/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h3>{{title}}</h3>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      title: 'Home 페이지'
+    }
+  },
 }
 </script>
