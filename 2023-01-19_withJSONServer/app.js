29d6ca2b2f7b1d41775c97bd13c2f03285204b64
sgchoi-dev <choi.sg.dev@gmail.com>
2023-01-26 16:59:09 +0900

    axios delete request 공부

--- 2023-01-19_withJSONServer/app.js
+++ 2023-01-19_withJSONServer/app.js
@@ -10,10 +10,17 @@ new Vue({
   methods: {
     showForm(emp){
       this.show = true;
+      if(emp !== undefined){
+        this.employee.id = emp.id;
+        this.employee.name = emp.name;
+        this.employee.email = emp.email;
+        this.isUpdate = true;
+      }
     },
     btnOK(){
       this.send = true;
-      if(this.isUpdate == false) this.addEmployee();
+      //if(this.isUpdate == false) this.addEmployee();
+      (this.isUpdate == false)? this.addEmployee() : this.updateEmployee(this.employee.id)
     },
     btnCancel(){
       this.show = false; this.send = false;
@@ -27,12 +34,32 @@ new Vue({
           this.show = false;
           this.send = false;
       }
+    },
+    updateEmployee(id){
+      axios.put('http://localhost:3000/employees/'+id,{
+        "id": this.employee.id,
+        "name": this.employee.name,
+        "email": this.employee.email,
+      })
+      .then((res)=>{
+        console.log(res); // 성공 시
+        this.isUpdate = false;
+      })
+      .catch((err)=>{
+        console.log(err); // 에러 발생 시
+      })
+    },
+    deleteEmployee(id){
+      console.log("id: " + id);
+      axios({method: 'delete', url:'http://localhost:3000/employees/'+id})
+      .then((res)=>{this.employee.splice(id, 1)})
+      .catch((err)=>console.log(err))
     }
   },
   created(){
     axios.get('http://localhost:3000/employees') // 서버에 요청하기
     .then((res)=>{
-      console.log(res); // 성공 시
+      //console.log(res); // 성공 시
       this.employees = res.data;
     })
     .catch((err)=>{console.log(err);}) // 에러 발생
